31aed8b3410fa47aaf0fdef9ca259fd98949fb68
PiotrFrugoton <piotr.turlej@frugoton.com>
2015-11-10 10:30:40 +0100

    changes in main.rb

--- main.rb
+++ main.rb
@@ -11,7 +11,8 @@ configure :development do
   DataMapper.setup(:default, 'sqlite:sinatra_service.db')
 end
 configure :production do
-  DataMapper.setup(:default, 'postgres://ivtgafmmrvwvrr:Py9QZC6sNWWtKc4LTaUYpURlyo@ec2-107-21-221-59.compute-1.amazonaws.com:5432/dkannidokv5du')
+  #DataMapper.setup(:default, 'postgres://ivtgafmmrvwvrr:Py9QZC6sNWWtKc4LTaUYpURlyo@ec2-107-21-221-59.compute-1.amazonaws.com:5432/dkannidokv5du')
+  DataMapper.setup(:default, ENV['DATABASE_URL'])
 end
 DataMapper.finalize
 DataMapper.auto_upgrade!
